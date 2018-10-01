bec08584e3b4eb7463947d96dc7ba71b5a441dc8
lilu <lilugirl2005@126.com>
2018-10-01 16:12:29 +0800

    Create game_stats.cpp

---
+++ Chapter01/game_stats.cpp
@@ -0,0 +1,25 @@
+// Game Stats
+// Demostrates declaring and initializing variables
+#include <iostream>
+using namespace std;
+int main()
+{
+  int score:
+  double distance;
+  char playAgain;
+  bool shieldsUp;
+  short lives,aliensKilled;
+  score=0;
+  distance=1200.76;
+  playAgain='y';
+  shieldsUp=true;
+  lives=3;
+  aliensKilled=10;
+  double engineTemp=6572.89;
+  cout << "\nscore:"  << score << endl;
+  cout << "distance:" << distance << endl;
+  cout << "playAgain:" << playAgain << endl;
+  // skipping shieldsUp since you don't generally print Boolean values
+  cout << "lives: " << lives << endl;
+  cout << "aliensKilled:" << aliensKiiled << endl;
+}
